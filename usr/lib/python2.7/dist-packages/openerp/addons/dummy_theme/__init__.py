07a75e87bbefcb14ea4790705890e7d83045965c
pcelardperez <pcelardperez@danielcastelao.org>
2015-03-27 13:00:30 +0100

    Commit de Odoo para el dummytheme

---
+++ usr/lib/python2.7/dist-packages/openerp/addons/dummy_theme/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "pedro"
+__date__ = "$11-mar-2015 9:55:00$"
+
+if __name__ == "__main__":
+    print "Hello World";
---
+++ usr/lib/python2.7/dist-packages/openerp/addons/dummy_theme/__openerp__.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+{
+    'name': 'Dummy Theme',
+    'description': """
+    """,
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+    'data': [
+        'views/pages.xml',
+        #'views/options.xml',
+        'views/snippets.xml',
+    ],
+    
+    'author': 'Pedro',
+    'website': "",
+}
+
